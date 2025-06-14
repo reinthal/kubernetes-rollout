fc206eeacf9867fcd5858d9cc83ff5acfef70dfc
Alexander Reinthal <email@reinthal.me>
2025-06-14 22:51:45 +0200

    feat(lb): add load-balancer for hetzner cluster

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,3 +7,8 @@ output "kubeconfig" {
   value     = talos_cluster_kubeconfig.this.kubeconfig_raw
   sensitive = true
 }
+
+output "worker_load_balancer_ip" {
+  value       = hcloud_load_balancer.worker_load_balancer.ipv4
+  description = "Public IP address of the worker load balancer for HTTPS traffic"
+}
