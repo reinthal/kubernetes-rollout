caab3894f36150ce1014f85c576bbc6e3b1242f8
Alexander Reinthal <email@reinthal.me>
2025-06-14 23:02:14 +0200

    refactor: tidy variables with claude

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,74 +1,106 @@
+# Hetzner Cloud Provider
 variable "hetzner_api_key" {
-  type = string
+  description = "Hetzner Cloud API key for authentication"
+  type        = string
+  sensitive   = true
 }
 
-# Talos specific variables
+variable "location" {
+  description = "Hetzner Cloud location for resources"
+  type        = string
+  default     = "fsn1"
+}
+
+variable "network_zone" {
+  description = "Hetzner Cloud network zone for load balancer"
+  type        = string
+  default     = "eu-central"
+}
+
+# Talos Configuration
 variable "image_id" {
-  type = string
-  default = "241897526"
+  description = "Hetzner snapshot ID for custom Talos OS image"
+  type        = string
+  default     = "241897526"
 }
 
 variable "cluster_name" {
-  description = "A name to provide for the Talos cluster"
+  description = "Name for the Talos cluster"
   type        = string
   default     = "talos-hloud-cluster"
 }
 
 variable "talos_version_contract" {
-  description = "Talos API version to use for the cluster, if not set the the version shipped with the talos sdk version will be used"
+  description = "Talos API version to use for the cluster"
   type        = string
   default     = "v1.6"
 }
 
 variable "kubernetes_version" {
-  description = "Kubernetes version to use for the cluster, if not set the k8s version shipped with the talos sdk version will be used"
+  description = "Kubernetes version to use for the cluster"
   type        = string
   default     = null
 }
 
-# Control plane
+# Control Plane Configuration
 variable "controlplane_type" {
-  default = "cx32"
+  description = "Hetzner server type for control plane nodes"
+  type        = string
+  default     = "cx32"
 }
 
 variable "controlplane_ip" {
-  default = "10.0.0.3"
-  type    = string
+  description = "Private IP address for the control plane node"
+  type        = string
+  default     = "10.0.0.3"
 }
 
-# Networking
+# Network Configuration
 variable "private_network_name" {
-  default = "talos-network"
+  description = "Name for the private network"
+  type        = string
+  default     = "talos-network"
 }
 
 variable "private_network_ip_range" {
-  default = "10.0.0.0/16"
+  description = "IP range for the private network"
+  type        = string
+  default     = "10.0.0.0/16"
 }
 
 variable "private_network_subnet_range" {
-  default = "10.0.0.0/24"
+  description = "IP range for the private network subnet"
+  type        = string
+  default     = "10.0.0.0/24"
 }
 
-# Load balancer
-variable "network_zone" {
-  default = "eu-central"
-}
-
+# Load Balancer Configuration
 variable "load_balancer_type" {
-  default = "lb11"
+  description = "Hetzner load balancer type"
+  type        = string
+  default     = "lb11"
 }
 
-# Workers
-variable "location" {
-  default = "fsn1"
-}
-
+# Worker Node Configuration
 variable "workers" {
-  description = "Worker definition"
+  description = "Map of worker node configurations"
+  type = map(object({
+    server_type = string
+    name        = string
+    location    = string
+    ip          = string
+    labels      = map(string)
+    taints      = list(string)
+  }))
 }
 
 variable "worker_extra_volume_size" {
-  description = "Size of SSD volume to attach to workers"
+  description = "Size in GB of additional SSD volume to attach to worker nodes"
   type        = number
   default     = 10
+
+  validation {
+    condition     = var.worker_extra_volume_size >= 10
+    error_message = "Worker extra volume size must be at least 10 GB."
+  }
 }
